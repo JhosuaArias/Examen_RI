b7933a2818c1955f858e9300342d3a0d9086a07b
JhosuaArias <jhosuarqui@hotmail.com>
2018-11-03 19:49:56 -0600

    comparacion_algoritmos just needs more content

--- Examen_RI/scripts/Distance_Calculator.js
+++ Examen_RI/scripts/Distance_Calculator.js
@@ -61,16 +61,6 @@ function computeLevenshteinDistance(string1, string2, stepByStep){
                 distance[i][j-1] + 1,       // Insertion
                 distance[i-1][j-1] + cost); // Substitution
 
-            if (stepByStep) {
-                console.log("");
-                console.log("Distance Matrix");
-                for (let subA in distance) {
-                    for (let n in subA) {
-                        console.log(n + " ");
-                    }
-                    console.log("");
-                }
-            }
         }
     }
 
@@ -125,4 +115,63 @@ function compareBigrams(uniqueBigramsString1, uniqueBigramsString2) {
 
 function computeBigramsDistanceFormula(commonBigramsAB, uniqueBigramsA, uniqueBigramsB) {
 	return ((2.0*commonBigramsAB)/(uniqueBigramsA+uniqueBigramsB));
+}
+
+
+function pressButtonCalcular(){
+    /**Getting all components**/
+    var component_word1 = document.getElementById("inword1");
+    var component_word2 = document.getElementById("inword2");
+
+    var component_Palabra1 = document.getElementById("word1"); 
+    var component_Palabra2 = document.getElementById("word2");
+
+    var component_hamming_distance = document.getElementById("hamming_distance");
+    var component_levenshtein_distance = document.getElementById("levenshtein_distance");
+    var component_bigrams_distance = document.getElementById("bigrams_distance");
+
+    var component_hamming_time = document.getElementById("hamming_time");
+    var component_levenshtein_time = document.getElementById("levenshtein_time");
+    var component_bigrams_time = document.getElementById("bigrams_time");
+
+
+       /**Setting words in the HTML document**/
+    var word1_text = component_word1.value;
+    var word2_text = component_word2.value;
+
+    component_Palabra1.innerHTML = word1_text;
+    component_Palabra2.innerHTML = word2_text;
+
+        /**Making LowerCase the words**/
+    word1_text = word1_text.toLowerCase();
+    word2_text = word2_text.toLowerCase();
+
+       /**Making distance and time vars**/
+    var hamming_distance;
+    var levenshtein_distance;
+    var bigrams_distance;
+
+    /** Computing distances here **/
+    var performance = window.performance;
+
+    const hamming_start_time = performance.now();
+    hamming_distance = computeHammingDistance(word1_text, word2_text);
+    const hamming_end_time = performance.now();
+
+    const levenshtein_start_time = performance.now();
+    levenshtein_distance = computeLevenshteinDistance(word1_text, word2_text,true);
+    const levenshtein_end_time = performance.now();
+
+    const bigrams_start_time = performance.now();
+    bigrams_distance = computeBigramsDistance(word1_text, word2_text);
+    const bigrams_end_time = performance.now();
+
+    /**Setting values in HTML document**/
+    console.log(hamming_end_time - hamming_start_time);
+    console.log(levenshtein_end_time - levenshtein_start_time);
+    console.log(bigrams_end_time - bigrams_start_time);
+
+    component_hamming_distance.innerHTML = hamming_distance;
+    component_levenshtein_distance.innerHTML = levenshtein_distance;
+    component_bigrams_distance.innerHTML = bigrams_distance;
 }
