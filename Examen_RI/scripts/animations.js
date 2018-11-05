892b969e0ef3e5c7efe819d7366de6490c474c71
Elías Calderón <eliascr98@outlook.com>
2018-11-04 22:29:29 -0600

    Added the tabs for the algorithms

---
+++ Examen_RI/scripts/animations.js
@@ -0,0 +1,33 @@
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+function openPage(pageName, elmnt, color) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Remove the background color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+        tablinks[i].style.color = 'black';
+    }
+
+    // Show the specific tab content
+    document.getElementById(pageName).style.display = "block";
+    document.getElementById(pageName).style.backgroundColor = color;
+
+    // Add the specific color to the button used to open the tab content
+    elmnt.style.backgroundColor = color;
+    elmnt.style.color = 'white';
+}
